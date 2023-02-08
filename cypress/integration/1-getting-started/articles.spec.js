294fafcc69a22465c6d78a99583ecc7df518f148
Timur Tsoi <timurtsoi@yahoo.com>
2023-02-08 11:15:28 -0600

    article

---
+++ cypress/integration/1-getting-started/articles.spec.js
@@ -0,0 +1,92 @@
+
+
+
+///<reference types="cypress" />
+
+import { login, openMyArticles } from '../../support/shared';
+import { faker } from '@faker-js/faker';
+
+function generateFakeArticle() {
+
+    return {
+        title: faker.lorem.sentence(),
+        description: faker.lorem.paragraph(),
+        tags: [
+            faker.word.adjective(),
+            faker.word.adjective(),
+            faker.word.adjective()
+        ]
+    };
+
+}
+
+describe('Articles', () => {
+
+    beforeEach(() => {
+        //cy.clearCookie();
+        cy.visit('/');
+        cy.get('.navbar').should('be.visible').as('appHeader');
+        login();
+        
+    });
+
+    it('should do publish article', () => {
+
+        // open editor
+        cy.get('@appHeader').find('a[href$="/editor/"]').click();
+        cy.url().should('include', '/#/editor/');
+
+        cy.get('.editor-page').as('addArticlePage');
+
+        cy.get('@addArticlePage').find('form')
+            .should('be.visible').as('addArticleForm');
+
+        const article = generateFakeArticle();
+
+        // fill and submit form
+        cy.get('@addArticleForm').find('input[ng-model$=title]').type(article.title);
+        cy.get('@addArticleForm').find('input[ng-model$=description]').type(article.description);
+        cy.get('@addArticleForm').find('input[ng-model$=tagField]').as('articleTagInput');
+        for (const tag of article.tags) {
+            cy.get('@articleTagInput').type(tag).type('{enter}');
+        }
+
+        const body = `I like fruits!
+It is **healthy** and *tasty.*
+
+My favorite are:
+* banana
+* orange
+* lemon`;
+        cy.get('@addArticleForm').find('textarea[ng-model$=body]').type(body);
+
+        // TODO: should be button[type=submit]
+        cy.get('@addArticleForm').find('button[type=button]').click();
+
+        // check article data
+        cy.get('.article-page').as('articlePage');
+        cy.get('@articlePage').find('h1').should('contains.text', article.title);
+
+        cy.get('@articlePage').find('.tag-list').as('articleTags');
+        for (const tag of article.tags) {
+            cy.get('@articleTags').should('contain.text', tag);
+        }
+
+        // check Markdown is rendered to HTML
+        cy.get('@articlePage').find('[ng-bind-html$=body]')
+            .should('contain.html', '<strong>healthy</strong>')
+            .should('contain.html', '<em>tasty.</em>')
+            .should('contain.html', '<li>banana</li>');
+
+        // TODO: check author & date
+
+    });
+     it.only ('open article', () =>{
+        openMyArticles()
+     })
+
+});
+
+
+
+
---
+++ test_cases/README.md
@@ -0,0 +1,11 @@
+This file contains test cases for [Conduit project](https://demo.realworld.io/)
+
+# Sign up
+* [x] register user
+* [x] login user
+* [x] logout user
+
+# Articles 
+* [ ] [publish article](publish_artical.md)
+* [ ] delete article
+* [ ] edit article
